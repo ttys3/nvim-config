a736c6f8f52b57a05d3fbab83d22c518c6087bad
荒野無燈 <ttys3@outlook.com>
2021-08-24 22:33:16 +0800

    chore: disable bold for func and keywords

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -515,16 +515,16 @@ return require("packer").startup {
 					fox = "nordfox", -- change the colorscheme to use nordfox
 					styles = {
 						comments = "italic", -- change style of comments to be italic
-						keywords = "bold", -- change style of keywords to be bold
-						functions = "italic,bold", -- styles can be a comma separated list
+						-- keywords = "bold", -- change style of keywords to be bold
+						-- functions = "italic,bold", -- styles can be a comma separated list
 					},
 					colors = {
 						red = "#FF000", -- Override the red color for MAX POWER
-						bg_alt = "#000000",
+						-- bg_alt = "#000000",
 					},
 					hlgroup = {
 						TSPunctDelimiter = { fg = "${red}" }, -- Override a highlight group with the color red
-						LspCodeLens = { bg = "#000000" },
+						-- LspCodeLens = { bg = "#000000" },
 					},
 				}
 
