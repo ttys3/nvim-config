8dabbe78eaa6f2e8f0a1ed2f11e83f26891407d3
ttys3 <ttys3@outlook.com>
2021-08-31 12:09:19 +0800

    chore: update lua/config/markdown-preview.lua theme

--- lua/config/markdown-preview.lua
+++ lua/config/markdown-preview.lua
@@ -1,6 +1,12 @@
 Variable.g {
 	-- do not close the preview tab when switching to other buffers
-	mkdp_auto_close = 0,
+	mkdp_auto_close = false,
+	mkdp_preview_options = {
+		maid = {
+			theme = "neutral",
+			-- theme = "dark",
+		},
+	},
 }
 nmap { "<leader>md", "<Plug>MarkdownPreview" }
 nmap { "<M-s>", "<Plug>MarkdownPreviewStop" }
