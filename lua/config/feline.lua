27deb1b485c011ee489aebd8477da84ebe26b7ad
ttys3 <ttys3@outlook.com>
2021-09-08 12:12:10 +0800

    fix(feline): avoid "attempt to index field 'left' (a nil value)" error in config

--- lua/config/feline.lua
+++ lua/config/feline.lua
@@ -8,12 +8,14 @@ require("feline.providers").add_provider("lsp_progress", function()
 	return #vim.lsp.buf_get_clients() > 0 and require("lsp").lsp_progress() or ""
 end)
 local components = require("feline.presets")["default"].components
-table.insert(components.left.active, {
-	provider = "lsp_client_names",
-	left_sep = " ",
-})
-table.insert(components.left.active, {
-	provider = "lsp_progress",
-	left_sep = " ",
-	-- right_sep = ' ',
-})
+if components.left then
+	table.insert(components.left.active, {
+		provider = "lsp_client_names",
+		left_sep = " ",
+	})
+	table.insert(components.left.active, {
+		provider = "lsp_progress",
+		left_sep = " ",
+		-- right_sep = ' ',
+	})
+end
