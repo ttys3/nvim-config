97e9e206aac03ba1febbd87c356e883e0b6fbff9
ttys3 <ttys3@outlook.com>
2021-09-26 12:31:00 +0800

    refactor(telescope): refine lsp_code_actions layout_strategy, set small width and height

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -7,7 +7,7 @@ nnoremap { "<leader><leader>", "<cmd>lua require('telescope.builtin').find_files
 nnoremap { "<leader>g", "<cmd>lua require('telescope.builtin').live_grep()<cr>" }
 nnoremap { "<leader>b", "<cmd>lua require('telescope.builtin').buffers()<cr>" }
 nnoremap { "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>" }
-nnoremap { "<leader>a", "<cmd>lua require('telescope.builtin').lsp_code_actions()<cr>" }
+nnoremap { "<leader>a", "<cmd>lua require('telescope.builtin').lsp_code_actions({layout_strategy='cursor',layout_config={width=50, height = 10}})<cr>" }
 
 local actions = require "telescope.actions"
 -- Global remapping
