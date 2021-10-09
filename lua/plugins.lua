be78205d3d897e20c052aec65c7c65f70d73d85d
荒野無燈 <ttys3@outlook.com>
2021-10-10 01:21:00 +0800

    refactor: replaced tpope/vim-commentary with numToStr/Comment.nvim
    Refs: https://www.reddit.com/r/neovim/comments/q35328/commentnvim_simple_and_powerful_comment_plugin/

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -323,12 +323,13 @@ return require("packer").startup {
 		-- " ]p pastes on the line below, [p pastes on the line above
 		use "tpope/vim-unimpaired"
 
-		-- " Use gcc to comment out a line (takes a count),
-		-- " gc to comment out the target of a motion (for example, gcap to comment out a paragraph),
-		-- " gc in visual mode to comment out the selection, and gc in operator pending mode to target a comment.
-		-- " You can also use it as a command, either with a range like :7,17Commentary, or
-		-- " as part of a :global invocation like with :g/TODO/Commentary. That's it.
-		use "tpope/vim-commentary"
+		use {
+			"numToStr/Comment.nvim",
+			config = function()
+				-- https://github.com/numToStr/Comment.nvim#configuration-optional
+				require("Comment").setup()
+			end,
+		}
 
 		-- Press + to expand the visual selection and _ to shrink it.
 		use "terryma/vim-expand-region"
