22d857a46ab3a3cc5617ea3581c182006cd7a0f8
ttys3 <ttys3@outlook.com>
2021-10-11 14:43:48 +0800

    refactor(feline): enable full-path for file_info component

--- lua/config/feline.lua
+++ lua/config/feline.lua
@@ -16,11 +16,32 @@ local components = require("feline.presets")["default"]
 if components.active[1] then
 	table.insert(components.active[1], {
 		provider = "lsp_client_names",
-		left_sep = " ",
+		left_sep = "  ",
 	})
 	table.insert(components.active[1], {
 		provider = "lsp_progress",
 		left_sep = " ",
 		-- right_sep = ' ',
 	})
+end
+
+-- enable full-path type for file_info
+-- https://github.com/famiu/feline.nvim/blob/3b46ef0bbf87732435ef54c1fef4f8cc9fceec3f/doc/feline.txt#L801
+-- https://github.com/famiu/feline.nvim/blob/3b46ef0bbf87732435ef54c1fef4f8cc9fceec3f/lua/feline/presets/default.lua#L27
+if components.active[1][3] and components.active[1][3].provider == "file_info" then
+	components.active[1][3].provider = {
+		name = "file_info",
+		opts = {
+			type = "full-path",
+		},
+	}
+
+	components.active[1][3].short_provider = {
+		name = "file_info",
+		opts = {
+			type = "short-path",
+		},
+	}
+	-- this will not work, see function parse_provider https://github.com/famiu/feline.nvim/blob/3b46ef0bbf87732435ef54c1fef4f8cc9fceec3f/lua/feline/generator.lua#L217
+	-- components.active[1][3].opts = { type = "full-path" }
 end
