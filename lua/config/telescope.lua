f76ac6ab1489f3638829e319114c5db9877405df
荒野無燈 <ttys3@outlook.com>
2021-10-20 04:18:34 +0800

    refactor: optimize telescope.lua buffers config

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -3,9 +3,15 @@
 -- This will load fzy_native and have it override the default file sorter
 require("telescope").load_extension "fzy_native"
 
+-- see https://github.com/nvim-telescope/telescope.nvim/blob/master/lua/telescope/pickers/layout_strategies.lua
 nnoremap { "<leader><leader>", "<cmd>lua require('telescope.builtin').find_files()<cr>" }
 nnoremap { "<leader>g", "<cmd>lua require('telescope.builtin').live_grep()<cr>" }
-nnoremap { "<leader>b", "<cmd>lua require('telescope.builtin').buffers()<cr>" }
+-- internal.buffers
+-- https://github.com/nvim-telescope/telescope.nvim/blob/9cad3a4a5d0e36b07b25c4be1db1c1306fcec945/lua/telescope/builtin/internal.lua
+nnoremap {
+	"<leader>b",
+	"<cmd>lua require('telescope.builtin').buffers({ignore_current_buffer = true, sort_mru = true, layout_strategy='vertical',layout_config={width=80}})<cr>",
+}
 nnoremap { "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>" }
 nnoremap { "<leader>a", "<cmd>lua require('telescope.builtin').lsp_code_actions({layout_strategy='cursor',layout_config={width=50, height = 10}})<cr>" }
 
