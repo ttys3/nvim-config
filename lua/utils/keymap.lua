5c5b69334ff8ecd399e87f45dd713eb54a23d54d
ttyS3 <ttys3.rust@gmail.com>
2022-01-19 23:11:39 +0800

    refactor: refine keymap.lua using new lua api: vim.keymap.set
    Refs: https://github.com/neovim/neovim/pull/16591

--- lua/utils/keymap.lua
+++ lua/utils/keymap.lua
@@ -2,20 +2,6 @@
 
 local keymap = {}
 
--- Have to use a global to handle re-requiring this file and losing all of the keymap.
---  In the future, the C code will handle this.
-__AstronautaMapStore = __AstronautaMapStore or {}
-keymap._store = __AstronautaMapStore
-
-keymap._create = function(f)
-    table.insert(keymap._store, f)
-    return #keymap._store
-end
-
-keymap._execute = function(id)
-    keymap._store[id]()
-end
-
 local make_mapper = function(mode, defaults, opts)
     local args, map_args = {}, {}
     for k, v in pairs(opts) do
@@ -30,149 +16,125 @@ local make_mapper = function(mode, defaults, opts)
     local rhs = opts.rhs or args[2]
     local map_opts = vim.tbl_extend("force", defaults, map_args)
 
-    local mapping
-    if type(rhs) == "string" then
-        mapping = rhs
-    elseif type(rhs) == "function" then
-        assert(map_opts.noremap, "If `rhs` is a function, `opts.noremap` must be true")
-
-        local func_id = keymap._create(rhs)
-        mapping = string.format([[<cmd>lua vim.keymap._execute(%s)<CR>]], func_id)
+    if type(rhs) == "string" or type(rhs) == "function" then
+        -- ok
     else
-        error("Unexpected type for rhs:" .. tostring(rhs))
+        error("Unexpected type for rhs:" .. type(rhs))
     end
 
-    if not map_opts.buffer then
-        vim.api.nvim_set_keymap(mode, lhs, mapping, map_opts)
-    else
-        -- Clear the buffer after saving it
-        local buffer = map_opts.buffer
-        if buffer == true then
-            buffer = 0
-        end
-
-        map_opts.buffer = nil
-
-        vim.api.nvim_buf_set_keymap(buffer, mode, lhs, mapping, map_opts)
+    if map_opts.buffer == true then
+        map_opts.buffer = 0
     end
+
+    vim.keymap.set(mode, lhs, rhs, map_opts)
 end
 
---- Helper function for ':map'.
+--- Helper function for ":map".
+-- mapmode-nvo
 function keymap.map(opts)
-    return make_mapper("", { noremap = false }, opts)
+    return make_mapper({"n", "v", "o"}, {}, opts)
 end
 
---- Helper function for ':noremap'
+--- Helper function for ":noremap"
 function keymap.noremap(opts)
-    return make_mapper("", { noremap = true }, opts)
+    return make_mapper({"n", "v", "o"}, { remap = false }, opts)
 end
 
---- Helper function for ':nmap'.
+--- Helper function for ":nmap".
 ---
 --- <pre>
----   keymap.nmap { 'lhs', function() print("real lua function") end, silent = true }
+---   keymap.nmap { "lhs", function() print("real lua function") end, silent = true }
 --- </pre>
---@param opts (table): A table with keys:
----     - [1] = left hand side: Must be a string
----     - [2] = right hand side: Can be a string OR a lua function to execute
----     - Other keys can be arguments to |:map|, such as "silent". See |nvim_set_keymap()|
----
 function keymap.nmap(opts)
-    return make_mapper("n", { noremap = false }, opts)
+    return make_mapper("n", {}, opts)
 end
 
---- Helper function for ':nnoremap'
+--- Helper function for ":nnoremap"
 --- <pre>
----   keymap.nmap { 'lhs', function() print("real lua function") end, silent = true }
+---   keymap.nmap { "lhs", function() print("real lua function") end, silent = true }
 --- </pre>
---@param opts (table): A table with keys
----     - [1] = left hand side: Must be a string
----     - [2] = right hand side: Can be a string OR a lua function to execute
----     - Other keys can be arguments to |:map|, such as "silent". See |nvim_set_keymap()|
----
----
 function keymap.nnoremap(opts)
-    return make_mapper("n", { noremap = true }, opts)
+    return make_mapper("n", { remap = false }, opts)
 end
 
---- Helper function for ':vmap'.
+--- Helper function for ":vmap".
 function keymap.vmap(opts)
-    return make_mapper("v", { noremap = false }, opts)
+    return make_mapper("v", {}, opts)
 end
 
---- Helper function for ':vnoremap'
+--- Helper function for ":vnoremap"
 function keymap.vnoremap(opts)
-    return make_mapper("v", { noremap = true }, opts)
+    return make_mapper("v", { remap = false }, opts)
 end
 
---- Helper function for ':xmap'.
+--- Helper function for ":xmap".
 function keymap.xmap(opts)
-    return make_mapper("x", { noremap = false }, opts)
+    return make_mapper("x", {}, opts)
 end
 
---- Helper function for ':xnoremap'
+--- Helper function for ":xnoremap"
 function keymap.xnoremap(opts)
-    return make_mapper("x", { noremap = true }, opts)
+    return make_mapper("x", { remap = false }, opts)
 end
 
---- Helper function for ':smap'.
+--- Helper function for ":smap".
 function keymap.smap(opts)
-    return make_mapper("s", { noremap = false }, opts)
+    return make_mapper("s", {}, opts)
 end
 
---- Helper function for ':snoremap'
+--- Helper function for ":snoremap"
 function keymap.snoremap(opts)
-    return make_mapper("s", { noremap = true }, opts)
+    return make_mapper("s", { remap = false }, opts)
 end
 
---- Helper function for ':omap'.
+--- Helper function for ":omap".
 function keymap.omap(opts)
-    return make_mapper("o", { noremap = false }, opts)
+    return make_mapper("o", {}, opts)
 end
 
---- Helper function for ':onoremap'
+--- Helper function for ":onoremap"
 function keymap.onoremap(opts)
-    return make_mapper("o", { noremap = true }, opts)
+    return make_mapper("o", { remap = false }, opts)
 end
 
---- Helper function for ':imap'.
+--- Helper function for ":imap".
 function keymap.imap(opts)
-    return make_mapper("i", { noremap = false }, opts)
+    return make_mapper("i", {}, opts)
 end
 
---- Helper function for ':inoremap'
+--- Helper function for ":inoremap"
 function keymap.inoremap(opts)
-    return make_mapper("i", { noremap = true }, opts)
+    return make_mapper("i", { remap = false }, opts)
 end
 
---- Helper function for ':lmap'.
+--- Helper function for ":lmap".
 function keymap.lmap(opts)
-    return make_mapper("l", { noremap = false }, opts)
+    return make_mapper("l", {}, opts)
 end
 
---- Helper function for ':lnoremap'
+--- Helper function for ":lnoremap"
 function keymap.lnoremap(opts)
-    return make_mapper("l", { noremap = true }, opts)
+    return make_mapper("l", { remap = false }, opts)
 end
 
---- Helper function for ':cmap'.
+--- Helper function for ":cmap".
 function keymap.cmap(opts)
-    return make_mapper("c", { noremap = false }, opts)
+    return make_mapper("c", {}, opts)
 end
 
---- Helper function for ':cnoremap'
+--- Helper function for ":cnoremap"
 function keymap.cnoremap(opts)
-    return make_mapper("c", { noremap = true }, opts)
+    return make_mapper("c", { remap = false }, opts)
 end
 
---- Helper function for ':tmap'.
+--- Helper function for ":tmap".
 function keymap.tmap(opts)
-    return make_mapper("t", { noremap = false }, opts)
+    return make_mapper("t", {}, opts)
 end
 
---- Helper function for ':tnoremap'
+--- Helper function for ":tnoremap"
 function keymap.tnoremap(opts)
-    return make_mapper("t", { noremap = true }, opts)
+    return make_mapper("t", { remap = false }, opts)
 end
 
 -- DO NOT USE `vim.keymap` !!! nvim 0.7.x now has builtin vim.keymap
