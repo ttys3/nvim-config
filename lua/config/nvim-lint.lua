b25ef4e4ed4f3e46eef6f0c8d4ac82be949603ea
ttyS3 <ttys3.rust@gmail.com>
2022-02-22 15:23:19 +0800

    fix: nvim-lint autocmd

--- lua/config/nvim-lint.lua
+++ lua/config/nvim-lint.lua
@@ -4,22 +4,34 @@ require("lint").linters_by_ft = {
 	go = { "golangcilint" },
 	dockerfile = { "hadolint" },
 }
+-- see https://github.com/dense-analysis/ale/blob/5b1044e2ade71fee4a59f94faa108d99b4e61fb2/autoload/ale/events.vim#L102
 Augroup {
 	NvimLint = {
 		{
-			"BufWritePost,BufEnter",
-			"<buffer>",
+			"BufEnter,BufRead",
+			"*",
 			function()
-				require("lint").try_lint()
+				vim.defer_fn(function()
+					require("lint").try_lint()
+				end, 300)
 			end,
 		},
-		-- InsertLeave or TextChanged
 		{
-			"InsertLeave",
+			"BufWritePost",
 			"*",
 			function()
 				require("lint").try_lint()
 			end,
 		},
+		-- InsertLeave or TextChanged
+		-- {
+		-- 	"InsertChange,TextChanged,TextChangedI",
+		-- 	"*",
+		-- 	function()
+		-- 		vim.defer_fn(function()
+		-- 			require("lint").try_lint()
+		-- 		end, 300)
+		-- 	end,
+		-- },
 	},
 }
