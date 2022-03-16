cc732ad4bd837b57b71b4f6311b3746c4e0af3c5
ttyS3 <ttys3.rust@gmail.com>
2022-03-17 03:13:14 +0800

    fix(colorscheme): fix nightfox config
    Refs: https://github.com/EdenEast/nightfox.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -510,25 +510,16 @@ return require("packer").startup {
 				-- it will be taken from the default configuration above
 				-- https://github.com/EdenEast/nightfox.nvim
 				nightfox.setup {
-					-- palefox, nightfox, nordfox
-					fox = "nordfox", -- change the colorscheme to use nordfox
-					styles = {
-						comments = "italic", -- change style of comments to be italic
-						-- keywords = "bold", -- change style of keywords to be bold
-						-- functions = "italic,bold", -- styles can be a comma separated list
-					},
-					colors = {
-						red = "#FF000", -- Override the red color for MAX POWER
-						-- bg_alt = "#000000",
-					},
-					hlgroup = {
-						TSPunctDelimiter = { fg = "${red}" }, -- Override a highlight group with the color red
-						-- LspCodeLens = { bg = "#000000" },
+					options = {
+						styles = {
+							comments = "italic", -- change style of comments to be italic
+							-- keywords = "bold", -- change style of keywords to be bold
+							-- functions = "italic,bold", -- styles can be a comma separated list
+						},
 					},
 				}
 
-				-- Load the configuration set above and apply the colorscheme
-				nightfox.load()
+				vim.cmd [[ silent! colorscheme nordfox ]]
 			end,
 		}
 		-- " one dark like colorscheme
