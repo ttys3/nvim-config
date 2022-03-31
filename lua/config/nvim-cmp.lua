cfc946e4c1cba8de8258162ee721151fc681bd1e
ttyS3 <ttys3.rust@gmail.com>
2022-03-31 18:06:55 +0800

    fix: fix up nvim-cmp go to function parameter with <Tab> not work problem

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -1,4 +1,13 @@
 -- Setup nvim-cmp.
+-- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
+
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
+
+local luasnip = require "luasnip"
+
 local cmp = require "cmp"
 
 cmp.setup {
@@ -35,14 +44,34 @@ cmp.setup {
 			-- behavior = cmp.ConfirmBehavior.Replace,
 			select = true,
 		},
-		-- https://github.com/hrsh7th/nvim-cmp/issues/231
-		["<Tab>"] = function(fallback)
+
+		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
 			else
 				fallback()
 			end
-		end,
+		end, {
+			"i",
+			"s",
+		}),
+
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, {
+			"i",
+			"s",
+		}),
 	},
 	sources = {
 		{ name = "nvim_lsp" },
