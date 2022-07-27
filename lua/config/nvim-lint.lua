6370f1ac3f1d6cf793f3ea9f982bf416280314df
ttyS3 <ttys3.rust@gmail.com>
2022-07-27 14:37:07 +0800

    chore: avoid duplicated error and warning overlay on the same line by lsp and nvim-lint both

--- lua/config/nvim-lint.lua
+++ lua/config/nvim-lint.lua
@@ -4,6 +4,16 @@ require("lint").linters_by_ft = {
 	go = { "golangcilint" },
 	dockerfile = { "hadolint" },
 }
+
+TryLint = function()
+	local diag = vim.diagnostic.get(0, { severity_limit = vim.diagnostic.severity.WARN })
+	if diag and type(diag) == "table" and #diag > 0 then
+		-- avoid duplicated error and warning overlay on the same line by lsp and nvim-lint both
+		return
+	end
+	require("lint").try_lint()
+end
+
 -- see https://github.com/dense-analysis/ale/blob/5b1044e2ade71fee4a59f94faa108d99b4e61fb2/autoload/ale/events.vim#L102
 Augroup {
 	NvimLint = {
@@ -12,7 +22,7 @@ Augroup {
 			"*",
 			function()
 				vim.defer_fn(function()
-					require("lint").try_lint()
+					TryLint()
 				end, 300)
 			end,
 		},
@@ -20,7 +30,7 @@ Augroup {
 			"BufWritePost",
 			"*",
 			function()
-				require("lint").try_lint()
+				TryLint()
 			end,
 		},
 		-- InsertLeave or TextChanged
