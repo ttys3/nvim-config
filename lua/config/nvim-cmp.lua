8546a558fa2416efc00f48d3d966301bbd876e68
ttyS3 <ttys3.rust@gmail.com>
2023-08-09 15:15:43 +0800

    fix: fix lazy.nvim loading issue (via inline require calls) on latest nvim iv0.10.0-dev version
    Refs: https://github.com/folke/lazy.nvim/discussions/583#discussioncomment-5060372

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -6,8 +6,6 @@ local has_words_before = function()
 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
 end
 
-local luasnip = require "luasnip"
-
 local cmp = require "cmp"
 
 cmp.setup {
@@ -50,8 +48,8 @@ cmp.setup {
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
+			elseif require("luasnip").expand_or_jumpable() then
+				require("luasnip").expand_or_jump()
 			elseif has_words_before() then
 				cmp.complete()
 			else
@@ -65,8 +63,8 @@ cmp.setup {
 		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
+			elseif require("luasnip").jumpable(-1) then
+				require("luasnip").jump(-1)
 			else
 				fallback()
 			end
