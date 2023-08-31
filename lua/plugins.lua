5c65a01f8b15dd75d91c7af0b405f3c644cfeb03
ttyS3 <ttys3.rust@gmail.com>
2023-09-01 04:41:44 +0800

    feat: add dap plugin ref to https://miguelcrespo.co/posts/how-to-debug-like-a-pro-using-neovim/

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -274,6 +274,51 @@ require("lazy").setup({
 		end,
 	},
 
+	-- https://miguelcrespo.co/posts/how-to-debug-like-a-pro-using-neovim/
+	-- https://github.com/mfussenegger/nvim-dap
+	-- go install github.com/go-delve/delve/cmd/dlv@latest
+	{
+		"mfussenegger/nvim-dap",
+		config = function ()
+			require('dap.ext.vscode').load_launchjs(nil, {})
+			vim.fn.sign_define('DapBreakpoint',{ text ='🟥', texthl ='', linehl ='', numhl =''})
+			vim.fn.sign_define('DapStopped',{ text ='▶️', texthl ='', linehl ='', numhl =''})
+
+			vim.keymap.set('n', '<F5>', require 'dap'.continue)
+			vim.keymap.set('n', '<F10>', require 'dap'.step_over)
+			vim.keymap.set('n', '<F11>', require 'dap'.step_into)
+			vim.keymap.set('n', '<F12>', require 'dap'.step_out)
+			vim.keymap.set('n', '<leader>b', require 'dap'.toggle_breakpoint)
+		end
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+		},
+		config = function ()
+			local dap, dapui =require("dap"),require("dapui")
+			dap.listeners.after.event_initialized["dapui_config"]=function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"]=function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"]=function()
+				dapui.close()
+			end
+		end
+	},
+	{
+		"leoluz/nvim-dap-go",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+		},
+		config = function()
+			require('dap-go').setup()
+		end,
+	},
+
 	-- Automatic input method switching for vim
 	-- https://github.com/rlue/vim-barbaric
 	-- auto switch to en on normal
