d410e579568c1a960e2369a3ff008965a133eee5
ttyS3 <ttys3.rust@gmail.com>
2025-04-08 06:13:42 +0000

    fix: remove smoka7/multicursors.nvim due to broken in Neovim v0.11
    https://github.com/smoka7/multicursors.nvim/issues/108

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -493,23 +493,6 @@ require("lazy").setup({
 		end,
 	},
 	{
-		"smoka7/multicursors.nvim",
-		event = "VeryLazy",
-		dependencies = {
-			"smoka7/hydra.nvim",
-		},
-		opts = {},
-		cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
-		keys = {
-			{
-				mode = { "v", "n" },
-				"<Leader>m",
-				"<cmd>MCstart<cr>",
-				desc = "Create a selection for selected text or word under the cursor",
-			},
-		},
-	},
-	{
 		"mfussenegger/nvim-lint",
 		config = function()
 			require "config.nvim-lint"
